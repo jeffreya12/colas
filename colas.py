c8231a93d62c1c1b60988147e4137c1c1bd25830
Jeffrey Alvarado <jefalva10296@gmail.com>
2017-06-13 02:12:11 +0000

    Fixes

--- colas.py
+++ colas.py
@@ -1,10 +1,17 @@
+# Lista con las lineas del archivo
 lines = []
+# Lista con cantidades de trabajo de cada cola
 a = []
+# Lista con cantidad de servidores de cada cola
 s = []
+# Lista con la tasa de atencion de cada cola
 miu = []
+# Lista con las probabilidades de pasar de una cola a otra
 p = []
+# Cantidad de colas
 m = int
 
+# Funcion que lee el archivo y lo asigna al la lista lines
 def leerArchivo(nombreArchivo):
     file = open(nombreArchivo, 'r')
     global lines
@@ -12,9 +19,10 @@ def leerArchivo(nombreArchivo):
     
     print(lines)
     
+# Funcion que inicializa los valores basicos necesarios
 def init():
+    global a, s, miu, p, m
     m = int(lines[0].split()[0])
-    #print(m)
     i = 1
     line = []
     while (i <= m):
@@ -22,22 +30,24 @@ def init():
         line = lines[i].split()
         while (j < 4):
             if (j == 0):
-                a.append(line[j])
+                a.append(float(line[j]))
             elif (j == 1):
-                s.append(line[j])
+                s.append(int(line[j]))
             elif (j == 2):
-                miu.append(line[j])
+                miu.append(float(line[j]))
             else:
-                p.append(line[j:])
+                p.append(list(map(float, line[j:])))
             j += 1
         i += 1
     
-    print(m)
-    print(a)
-    print(s)
-    print(miu)
-    print(p)
+
         
     
 leerArchivo("prueba.txt")
 init()
+
+print(m)
+print(a)
+print(s)
+print(miu)
+print(p)
