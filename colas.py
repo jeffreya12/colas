33f829e3ca2593dc0c14d6691621609ddf425f8b
jarivas <jarivas@ic-itcr.ac.cr>
2017-06-12 19:49:17 -0600

    Se agrego el calculo de lamda, además se conviertieron los parametros de entrada a floats.

--- colas.py
+++ colas.py
@@ -1,43 +1,85 @@
+import sys
+import numpy as np
+
 lines = []
-a = []
-s = []
-miu = []
-p = []
-m = int
+clientes = []                #a:j
+servidores = []              #s:i
+miu = []                     #u:i
+probabilidades = []          #P:ij
+colas = int                  #m
+pSalir = float               #probabilidades de salir
 
 def leerArchivo(nombreArchivo):
     file = open(nombreArchivo, 'r')
     global lines
     lines = file.readlines()
-    
-    print(lines)
+    print("\n\r\n")
+    #print(lines)
     
 def init():
-    m = int(lines[0].split()[0])
+    colas = int(lines[0].split()[0])
     #print(m)
     i = 1
     line = []
-    while (i <= m):
+    while (i <= colas):
         j = 0
         line = lines[i].split()
         while (j < 4):
             if (j == 0):
-                a.append(line[j])
+                clientes.append(float(line[j]))
             elif (j == 1):
-                s.append(line[j])
+                servidores.append(line[j])
             elif (j == 2):
                 miu.append(line[j])
             else:
-                p.append(line[j:])
+                probabilidades.append([float(item) for item in line[j:]])
             j += 1
         i += 1
     
-    print(m)
-    print(a)
-    print(s)
-    print(miu)
-    print(p)
-        
+    print("Cantidad de colas:                       ",colas)
+    print("Servidores s:j:                          ",servidores)
+    print("Ts.. atencion por unidad de tiempo u:j   ",miu)
+    print("Trabajos/Clientes a:j:                   ",clientes)    
+    print("Probabilidades P:ij:                     ",probabilidades)
     
-leerArchivo("prueba.txt")
-init()
+
+def calcularSalida():
+    valor = 0
+    
+    b = np.matrix(clientes) 
+    b = b.transpose()
+    b = -b
+    a = np.matrix(probabilidades)
+    
+    #Coloca en diagonal -1 en la matriz de a
+    for i in range(len(a)):
+		a[i,i] = -1
+	#Resuelve la ecuacion lineal para obtener valores de lambda
+    lamdas = np.linalg.solve(a,b)
+    for i in range(len(lamdas)):
+		print("Lambda ", i, ": ", lamdas[i,0])
+    for e in probabilidades:
+		
+        for i in e:
+            valor+= float(i)
+    pSalir = 1 - valor
+    if pSalir < 0:
+        pSalir = 0
+    print("Probabilidad de salir q:i:               ",pSalir)
+        
+def main(): 
+    print("\r\n\r\n\r\n")
+    print("--------------------------\n--------------------------")
+    print("-Ordenando Datos:-")
+    print("--------------------------\n--------------------------\n")   
+    leerArchivo(sys.argv[1])
+    init()
+    calcularSalida()
+    print("\r\n\r\n\r\n")
+
+
+if __name__ == "__main__":
+    main()
+
+#Para correr archivo
+#python colas.py prueba.txt
